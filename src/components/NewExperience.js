d9cd49111b84ff869a2a606652f04202581f1485
Liz <elizabeth.pinero.r@gmail.com>
2022-09-03 11:34:00 -0400

    Added new files.

---
+++ src/components/NewExperience.js
@@ -0,0 +1,30 @@
+import {Component } from 'react'
+
+export default class NewExperience extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        const {experience} = this.props;
+        return(
+            <div className='experience-container'>
+                {experience.experienceArr.map((obj) => {
+                    return (
+                        <div key={obj.id} className='new-experience'>
+                            <div className='section-left'><h3>{obj.role}</h3></div>
+                            <div className='section-right'>
+                                <h4>{obj.company}</h4>
+                                <p>{obj.dateStart}</p>
+                                <p>{obj.dateEnd}</p>
+                                <p>{obj.description}</p>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+}
+
